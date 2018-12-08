3ba3bfd319644afec84737ed2172b6031afe02a8
Alex Ayala <alexayala9975@hotmail.com>
2018-12-08 15:45:44 -0800

    Add comments

--- code_force.py
+++ code_force.py
@@ -8,7 +8,7 @@ import requests
 import os
 
 CONTEST_LIST_API = "http://codeforces.com/api/contest.list?gym=%s"
-SUBMISSION_LIST_API = "https://codeforces.com/api/contest.status?contestId=%s&count=100"
+SUBMISSION_LIST_API = "https://codeforces.com/api/contest.status?contestId=%s&count=10"
 CONTEST_URL = "http://codeforces.com/contest/%s/status"
 GYM_URL = "http://codeforces.com/gym/%s/status/"
 SUBMISSION_URL = "https://codeforces.com/contest/%s/submission/%s"
@@ -21,38 +21,35 @@ MEMORY_INDEX = 6
 
 
 def scrape_code_force(gym=False):
-    contest_list_query = CONTEST_LIST_API % str(gym).lower()
-    code_page_url = GYM_URL if gym else CONTEST_URL
-
     base_download_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "problem_data")
-    if not os.path.isdir(base_download_path):
-        os.mkdir(base_download_path)
+    contest_list_query = CONTEST_LIST_API % str(gym).lower()
 
-
-    i = 0
+    # Queue to hold id's of contests
     contest_id_queue = Queue()
+    # Process an api call into a dictionary
     contest_list_dict = json.loads(requests.get(contest_list_query).text)
+
     for contest_dict in contest_list_dict["result"]:
+        # If the contest is finished we'll include it
         if contest_dict["phase"] == "FINISHED":
             contest_id_queue.put(contest_dict['id'])
 
-            i+=1
-            if i == 1:
-                break
-        
     print("Contests found: %s" % contest_id_queue.qsize())
     
-    # TODO: Implement per contest per thread
+    # Queue will hold contest id's and submission id's
     submission_id_queue = Queue()
     while not contest_id_queue.empty():
         contest_id = contest_id_queue.get_nowait()
-        submission_list_request = requests.get(SUBMISSION_LIST_API % contest_id)
-        submission_list_dict = json.loads(submission_list_request.text)
+        # Process the API request (this takes a while, anything we can do about this?)
+        submission_list_dict = json.loads(requests.get(SUBMISSION_LIST_API % contest_id).text)
+
+        if submission_list_dict["status"] == "FAILED":
+            continue
 
         for submission_dict in submission_list_dict['result']:
-            if submission_dict['verdict'] != "OK" or "C++" not in submission_dict['programmingLanguage']:
-                continue
-            submission_id_queue.put((str(contest_id), str(submission_dict['id'])))
+            # If the submission is valid and is written in C++, add it to the queue
+            if submission_dict['verdict'] == "OK" and "C++" in submission_dict['programmingLanguage']:
+                submission_id_queue.put((str(contest_id), str(submission_dict['id'])))
     
     print("Submissions found: %s" % submission_id_queue.qsize())
 
@@ -66,6 +63,7 @@ def scrape_code_force(gym=False):
     making_requests = True
 
     # Queue that requests will be entered to
+    # These will start running immediately and wait for the queue while making_requests is true
     request_queue = Queue()
     for _ in range(3):
         t = threading.Thread(target=thread_process_request_queue, args=(base_download_path, request_queue))
@@ -75,23 +73,28 @@ def scrape_code_force(gym=False):
     while not submission_id_queue.empty():
         contest_id, submission_id = submission_id_queue.get_nowait()
 
-        problem_folder = os.path.join(base_download_path, contest_id)
-        if not os.path.isdir(problem_folder):
-            os.mkdir(problem_folder)
+        # Make the folder problem_data/contest_id if it doesnt exist
+        contest_folder = os.path.join(base_download_path, contest_id)
+        if not os.path.isdir(contest_folder):
+            os.mkdir(contest_folder)
         
+        # Get the submission page and dump the page into the queue
         page_url = SUBMISSION_URL % (contest_id, submission_id)
         r = requests.get(page_url)
         request_queue.put(r.text)
     
     print("All requests have been made. Processing remaining requests...")
     
+    # Let threads know that no more requests will be added to the queue
     making_requests = False
+    # Let the main thread help out
     thread_process_request_queue(base_download_path, request_queue)
     while threading.active_count() != 1:
         continue
     
 
 def thread_process_request_queue(base_download_path, request_queue):
+    # Let threads know that requests are still being made
     global making_requests
     while True:
         try:
@@ -104,52 +107,70 @@ def thread_process_request_queue(base_download_path, request_queue):
             else:
                 return
         
+        # Process the page into a soup
         soup = BeautifulSoup(request_text, 'html.parser')
+
+        # Get the first table and the second row of said table
+        # Then get all columns of the row
         submission_data = soup.find('table').find_all('tr')[1].find_all('td')
 
+        # Get submission id
         submission_id = submission_data[SUBMISSION_INDEX].text
 
+        # Get the problem name in the format XXXXY where X is the contest id and Y
+        # is the problem letter
         problem_name = submission_data[PROBLEM_INDEX].text.lower()
         problem_name = problem_name.split(" -")[0]
         problem_name = problem_name.lstrip()
         problem_name = problem_name.rstrip()
         
+        # Split into XXXX and Y here
         contest_id = problem_name[:-1]
         problem_id = problem_name[-1]
 
+        # Get the language that the submission was written in
         code_lang = submission_data[LANG_INDEX].text.lower()
         code_lang = code_lang.rstrip()
         code_lang = code_lang.lstrip()
         code_lang = re.sub(r'  +', r'', code_lang)
         code_lang = code_lang.replace(" ", '_')
+        # If there's a version number after c++, i.e, c++ 11, we replace
+        # the ++ and space with pp_, otherwise just pp
         if code_lang[-1] == '+':
             code_lang = code_lang.replace("++", "pp")
         else:
             code_lang = code_lang.replace("++", "pp_")
 
 
+        # Get the elapsed time of the solution
         code_time = submission_data[TIME_INDEX].text
         code_time = code_time.lstrip()
         code_time = code_time.rstrip()
         code_time = code_time[:-3]
 
+        # Get the memory used by the solution
         code_memory = submission_data[MEMORY_INDEX].text
         code_memory = code_memory.lstrip()
         code_memory = code_memory.rstrip()
         code_memory = code_memory[:-3]
 
+        # Find the source code
         source_code = soup.find('pre', {'id': "program-source-text"})
 
+        # If the problem letter folder doesnt exist, make it
         problem_path = os.path.join(base_download_path, contest_id, problem_id)
         if not os.path.isdir(problem_path):
             os.mkdir(problem_path)
         
+        # Paths for the code and info on it
         file_path = os.path.join(problem_path, submission_id + '.cpp')
         info_path = os.path.join(problem_path, submission_id + '.yaml')
 
+        # Write the source code
         with open(file_path, 'wb') as source_code_file:
             source_code_file.write(source_code.text.encode())
         
+        # Write info about the code
         with open(info_path, 'w') as submission_info_file:
             submission_info_file.write('lang: %s\n' % code_lang)
             submission_info_file.write('time: %s\n' % code_time)
