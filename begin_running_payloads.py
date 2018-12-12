f7c7a749ea4bb49969e6aeee661281307de2d4ff
ayalaa2 <ayalaa2@wwu.edu>
2018-12-11 16:49:56 -0800

    Fix connecting to machines

--- begin_running_payloads.py
+++ begin_running_payloads.py
@@ -1,21 +1,23 @@
 
 from paramiko.client import SSHClient
-
+from paramiko import AutoAddPolicy
 
 COMPUTER_COUNT = 50
-PAYLOAD_COUNT = 100
+PAYLOAD_COUNT = 5
 LABS = ["162", "164", "165", "167", "405", "418", "420"]
 BASE_SSH_NAME = "cf%s-%s"
 
 
 def main():
     ssh_connections = [None] * PAYLOAD_COUNT
+    computer_session_gen = get_next_computer_session()
 
     while True:
         for i in range(PAYLOAD_COUNT):
             if ssh_connections[i] is None or not session_is_active(ssh_connections[i]):
-                ssh_connections[i] = get_next_computer_session()
+                ssh_connections[i] = next(computer_session_gen)
                 # do more stuff
+                ssh_connections[i].exec_command("touch %s.test" % str(i))
             
 
 def session_is_active(ssh_client):
@@ -36,21 +38,33 @@ def get_next_computer_session():
 
     ssh_client = SSHClient()
     ssh_client.load_system_host_keys()
+    ssh_client.set_missing_host_key_policy(AutoAddPolicy())
 
     while True:
         for lab_name in LABS:
             for computer_index in computer_indices:
                 ssh_name = BASE_SSH_NAME % (lab_name, computer_index)
+                print("Trying %s..." % ssh_name)
 
                 try:
-                    ssh_client.connect(ssh_name)
+                    ssh_client.connect(ssh_name, timeout=1.0, port=922)
                 except:
                     print("Can't connect to %s, skipping..." % ssh_name)
                     continue
                 
                 print("Connected to %s" % ssh_name)
-
+                
                 yield ssh_client
                 
                 ssh_client = SSHClient()
                 ssh_client.load_system_host_keys()
+                ssh_client.set_missing_host_key_policy(AutoAddPolicy())
+
+
+if __name__ == "__main__":
+    main()
+    #a = SSHClient()
+    #a.load_system_host_keys()
+    #a.set_missing_host_key_policy(AutoAddPolicy())
+    #a.connect("cf405-01", timeout=1.0, port=922)
+
